fb1772b466155832652e556eb086a237293a1b2e
sandesh <rajputsandesh726@gmail.com>
2025-06-03 08:38:14 +0530

    code updated

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function click(){
-//let btn = document.getElementById("inputbtn");
-console.log("button clicked")
+function checkPassword(){
+const inputPassword = document.getElementById("inputPassword").value.trim();
+const message = document.getElementById("message");
+
+let fullPattern = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~`])/;
+
+  if (inputPassword.length < 8) {
+    message.textContent = "Password must be at least 8 characters long.";
+    message.style.color = "red";
+    return;
+  }
+
+  if (fullPattern.test(inputPassword)) {
+    message.textContent = "Strong password!";
+    message.style.color = "green";
+  } else {
+    message.textContent = "Password must include uppercase, lowercase, number, and symbol.";
+    message.style.color = "orange";
+  }
 }
 
-document.getElementById("inputbtn").addEventListener("click", click);
+document.addEventListener("click", checkPassword);
